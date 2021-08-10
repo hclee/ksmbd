a3c789a6026bb07fa2e5f6c3a46cc20e9b9e4042
Hyunchul Lee <hyc.lee@gmail.com>
2021-08-10 16:22:51 +0900

    ksmbd: smbd: init mr pool if needed
    For some RDMA NICs, memory registration + RDMA read/write
    is better than only RDMA read/write, and rdma_rw_ctx_init()
    needs the memory registeration in this situation.
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- transport_rdma.c
+++ transport_rdma.c
@@ -414,6 +414,7 @@ static void free_transport(struct smb_direct_transport *t)
 
 	if (t->qp) {
 		ib_drain_qp(t->qp);
+		ib_mr_pool_destroy(t->qp, &t->qp->rdma_mrs);
 		ib_destroy_qp(t->qp);
 	}
 
@@ -1787,6 +1788,7 @@ static int smb_direct_create_qpair(struct smb_direct_transport *t,
 {
 	int ret;
 	struct ib_qp_init_attr qp_attr;
+	int pages_per_rw, pages_per_mr, mr_count;
 
 	t->pd = ib_alloc_pd(t->cm_id->device, 0);
 	if (IS_ERR(t->pd)) {
@@ -1833,6 +1835,21 @@ static int smb_direct_create_qpair(struct smb_direct_transport *t,
 
 	t->qp = t->cm_id->qp;
 	t->cm_id->event_handler = smb_direct_cm_handler;
+
+	pages_per_rw = DIV_ROUND_UP(t->max_rdma_rw_size, PAGE_SIZE) + 2;
+	if (pages_per_rw > t->cm_id->device->attrs.max_sgl_rd) {
+		pages_per_mr = min_t(int, pages_per_rw,
+				     t->cm_id->device->attrs.max_fast_reg_page_list_len);
+		mr_count = DIV_ROUND_UP(pages_per_rw, pages_per_mr) *
+			atomic_read(&t->rw_avail_ops);
+		ret = ib_mr_pool_init(t->qp, &t->qp->rdma_mrs, mr_count,
+				      IB_MR_TYPE_MEM_REG, pages_per_mr, 0);
+		if (ret) {
+			pr_err("failed to init mr pool count %d pages %d\n",
+			       mr_count, pages_per_mr);
+			goto err;
+		}
+	}
 
 	return 0;
 err:
